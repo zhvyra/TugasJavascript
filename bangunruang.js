b27223c21cf3d18a717a585c2aeee392e5e1f1ae
Zhafira Aghniya Rahman <firaaaaa16@gmail.com>
2023-03-09 23:26:18 +0700

    revisi dikit bangunruang.js

--- bangunruang.js
+++ bangunruang.js
@@ -1,13 +1,7 @@
+
+//Luas Persegi Panjang
 const LuasPersegiPanjang = (panjang, lebar) => {
     return panjang * lebar
-}
-
-const LuasJajarGenjang = (alas, tinggi) => {
-    return alas * tinggi
-}
-
-const LuasSegitiga = (alas, tinggi) => {
-    return (alas * tinggi) / 2
 }
 
 const panjangpersegipanjang = prompt("Masukan Panjang Persegi Panjang : ")
@@ -15,12 +9,18 @@ const lebarpersegipanjang = prompt("Masukan Lebar Persegi Panjang : ")
 
 console.log("Luas Persegi Panjang = ", LuasPersegiPanjang(panjangpersegipanjang, lebarpersegipanjang))
 
+
+//Luas Jajar Genjang
+const LuasJajarGenjang = (alas, tinggi) => {
+    return alas * tinggi
+}
+
 const alasjajargenjang = prompt("Masukan Alas Jajar Genjang : ")
 const tinggijajargenjang = prompt("Masukan Tinggi Jajar Genjang : ")
 
 console.log("Luas Jajar Genjang = ", LuasJajarGenjang(alasjajargenjang, tinggijajargenjang))
 
-const alassegitiga = prompt("Masukan Alas Segitiga : ")
-const tinggisegitiga = prompt("Masukan Tinggi Segitiga : ")
 
-console.log("Luas Segitiga = ", LuasSegitiga(alassegitiga,tinggisegitiga))
+//Luas Segitiga
+const LuasSegitiga = (alas, tinggi) => {
+    return (alas * tinggi) / 2
